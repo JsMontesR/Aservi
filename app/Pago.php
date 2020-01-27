06b0a642b91fee01f9df31a4882c643c9b104114
Sebastian Montes <sebasmontes@live.com>
2020-01-27 16:34:35 -0500

    Tablas coloración

--- app/Pago.php
+++ app/Pago.php
@@ -9,7 +9,7 @@ use App\User;
 class Pago extends Model
 {
     protected $table = 'pagos';
-    protected $fillable = ['id',,'afiliacion_id'];
+    protected $fillable = ['id','afiliacion_id'];
 
     public function afiliacion(){
     	return $this->belongsTo(Afiliacion::class);
