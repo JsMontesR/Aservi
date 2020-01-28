0c6d4b781d498eaf2b7b953ef688f9aeecf04c13
Sebastian Montes <sebasmontes@live.com>
2020-01-28 12:26:42 -0500

    Tablas coloración

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,11 +125,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
+                    
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -137,6 +133,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            @if (Route::has('register'))
+                            <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            @endif
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
@@ -146,6 +145,7 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
+                            
                         </div>
                     </li>
                 @endguest
